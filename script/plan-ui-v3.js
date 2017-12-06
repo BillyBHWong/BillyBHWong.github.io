7d0a7b0655b5dfc0edb663f5a9f2cc978762bef5
BillyBHWong <31603761+BillyBHWong@users.noreply.github.com>
2017-12-06 09:01:19 -0500

    Update plan-ui-v3.js

--- script/plan-ui-v3.js
+++ script/plan-ui-v3.js
@@ -33,7 +33,7 @@ $("#form-submit").on("click", function(){
     A-2: First Floor
     A-X: Roof
     */
-    //draw plan in corresponding grid using
+    //draw plan in corresponding grid using SVG
     for (var i = 0; i < pFloor + 2; i++) {
         
         //recreate proper DOM elements for typical floors
@@ -42,7 +42,22 @@ $("#form-submit").on("click", function(){
         $(".main-grid").append($div);
         
         //draw svg wrapping div's
-        displayFloor(prop, allPlan[i]);
+        var $draw = SVG(allPlan[i].name);
+        $draw.viewbox(0, 0, $(".grid").width(), $(".grid").height());
+        // draw background
+        var background = $draw.rect($(".grid").width(), $(".grid").height()).fill("#fff");
+        
+        //first draw property line
+        displayPropLine($draw, prop);
+        //then draw plan outline
+        displayFloor($draw, prop, allPlan[i]);
+        //draw stairs where applicable
+        if (i == pFloor + 1 && $("#roofAccess").is(":checked") === false) {
+        } else {
+            displayStair($draw, allPlan[i]);
+        }
+        //display info of floor
+        displayFloorInfo($draw, allPlan[i]);        
     }
 })
 
@@ -52,6 +67,26 @@ function init_p_input(){
     pRear = Number($("#rear-yard").val());
     pSetback = 0;
     pFloor = Number($("#story-num").val());
+}
+
+//redraw plans when input chagnes
+function initPlans(){
+    for (var i = 0; i < pFloor + 2; i++){
+        //push plan geometry class into array and assign name
+        allPlan.push(new flrTyp());
+        
+        allPlan[i].iden = i;
+        
+        if (i == 0) {
+            allPlan[i].name = "Basement";
+        } else if (i == pFloor + 1) {
+            allPlan[i].name = "Roof";
+        } else {
+            allPlan[i].name = toOrdinal(i) + " Floor";
+        }
+        
+        flrCalc(allPlan[i]);
+    }
 }
 
 //mouse wheel scrolling event
@@ -115,48 +150,40 @@ function flrCalc(f) {
     
 }
 
-//takes in property(p) and floor plan(p) objects as arguments and draw, need to further separate into draw functions instead of one giant function, displayInfo, displayPropLine, displayFloor, displayRooms, displayDims
-function displayFloor(p, f) {
-    
-    //prepare canvas
+//takes in SVG canvas (d), property(p), and floor plan(p) objects as arguments and draw, need to further separate into draw functions instead of one giant function, displayInfo, displayPropLine, displayFloor, displayRooms, displayDims
+function displayPropLine(d, p) {
+    //recognize SVG canvas
+    var draw = d;
     var w = $(".grid").width();
     var h = $(".grid").height();
-    //declare center of canvas
+    
     p.posX = (w - p.w) / 2;
     p.posY = (h - p.d) / 2;
-    
-    // create SVG document and set its size
-    var draw = SVG(f.name);
-    //set viewbox
-    draw.viewbox(0, 0, w, h);
-    // draw background
-    var background = draw.rect(w, h).fill("#fff");
-    
-    //create floor plan label and square footage
-    var floorInfo = draw.text(f.name + "\n" + f.area + " sqft").font({
-        size: 14,
-        family: 'Menlo, sans-serif',
-        anchor: 'middle',
-        fill: '#111'
-    }).move(w/2, h-50);
     
     //locate and draw property line
     var prop = draw.rect(p.w, p.d)
         .x(p.posX).y(p.posY)
         .fill("#fff")
         .stroke({ width: 1, color: '#111', dasharray: '10,8,5,8'});
+}
+
+function displayFloor(d, p, f) {
+    //recognize SVG canvas
+    var draw = d;
     
+    f.posX = p.posX;
+    f.posY = p.posY + ft_to_in(pRear);
     //locate and draw plan
     var floor = draw.rect(f.w, f.d)
-        .x(p.posX).y(p.posY + ft_to_in(pRear))
+        .x(f.posX).y(f.posY)
         .fill("#fff")
         .stroke({ width: f.wallExtThick, color: "#000"});
     
     //draw dimension strings (needs find a way to shorten this code)
-    var longDim = draw.line(p.posX + f.w/4,
-                            p.posY + ft_to_in(pRear),
-                            p.posX + f.w/4,
-                            p.posY + ft_to_in(pRear) + f.d)
+    var longDim = draw.line(f.posX + f.w/4,
+                            f.posY,
+                            f.posX + f.w/4,
+                            f.posY + f.d)
         .stroke({ width: 1 , color: "#111"});
     
     longDim.marker('start', 10, 10, function(add) {
@@ -167,10 +194,10 @@ function displayFloor(p, f) {
         add.line(0, 0, 10, 10).stroke({ width: 1 , color: "#111"});
     })
     
-    var shortDim = draw.line(p.posX,
-                            p.posY + ft_to_in(pRear) + f.d/4,
-                            p.posX + f.w,
-                            p.posY + ft_to_in(pRear) + f.d/4)
+    var shortDim = draw.line(f.posX,
+                            f.posY + f.d/4,
+                            f.posX + f.w,
+                            f.posY + f.d/4)
         .stroke({ width: 1 , color: "#111"});
     
     shortDim.marker('start', 10, 10, function(add) {
@@ -182,22 +209,39 @@ function displayFloor(p, f) {
     })
 }
 
-//redraw plans when input chagnes
-function initPlans(){
-    for (var i = 0; i < pFloor + 2; i++){
-        //push plan geometry class into array and assign name
-        allPlan.push(new flrTyp());
-        
-        allPlan[i].iden = i;
-        
-        if (i == 0) {
-            allPlan[i].name = "Basement";
-        } else if (i == pFloor + 1) {
-            allPlan[i].name = "Roof";
-        } else {
-            allPlan[i].name = toOrdinal(i) + " Floor";
-        }
-        
-        flrCalc(allPlan[i]);
+function displayFloorInfo(d, f) {
+    //recognize SVG canvas
+    var draw = d;
+    
+    //create floor plan label and square footage
+    var floorInfo = draw.text(f.name + "\n" + f.area + " sqft").font({
+        size: 14,
+        family: 'Menlo, sans-serif',
+        anchor: 'middle',
+        fill: '#111'
+    }).move($(".grid").width()/2, $(".grid").height()-50);
+}
+
+function displayStair(d, f) {
+    
+    var draw = d;
+    
+    var flrHeight = ft_to_in(10);
+    var width = ft_to_in(3);
+    var rise = 6;
+    var tread = 11;
+    var step = flrHeight / rise;
+    var length = step * tread;
+    var x = f.posX + f.w - width;
+    var y = f.posY + (f.d - length)/2;
+    
+    var stairs = draw.rect(width, length)
+        .x(x).y(y)
+        .fill("none")
+        .stroke({ width: 2, color: '#000'})
+    
+    for (var i = 0; i < step; i++) {
+        var steps = draw.line(x, y + i * tread, x + width, y + i * tread)
+        .stroke({ width: 1, color: '#000'})
     }
 }
